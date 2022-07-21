1aefba3618006635c3a43139b0b3dbaca0919386
sheawinkler <sheawinkler@gmail.com>
2022-07-20 19:39:51 -0600

    working ema calculator - need to record all values next

--- main.py
+++ main.py
@@ -26,15 +26,16 @@ df.set_index('date', inplace=True)
 ## TODO: perform analysis of data (LSTM, ema cross, etc.)
 ema_6_init = eth[0]
 ema_9_init = eth[0]
-def ema6_calculatoor(coin_price_list, ema = 0, alpha = 0.167):
-    if len(coin_price_list) == 0:
-        return ema
-    if ema == 0:
+def ema6_calculatoor(coin_price_list, ema = 0.0, alpha = 0.167):
+    if ema == 0.0:
         ema = coin_price_list.pop(0)
-        ema6_calculatoor(coin_price_list, ema=ema)
+        return ema6_calculatoor(coin_price_list, ema=ema)
     else:
-        ema = ema + alpha(coin_price_list.pop(0) - ema)
-        ema6_calculatoor(coin_price_list, ema=ema)
+        ema = ema + alpha * (coin_price_list.pop(0) - ema)
+        if len(coin_price_list) == 0:
+            return ema
+        else:
+            return ema6_calculatoor(coin_price_list, ema=ema)
 
     
 
