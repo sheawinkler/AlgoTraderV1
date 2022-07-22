3c04a9bd8499015a1f56be6a17593143e6d27172
sheawinkler <sheawinkler@gmail.com>
2022-07-21 20:06:15 -0600

    functionalizing and organizing

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AlgoTraderV1
+
+### data source
+* Currently using CoinGeckoAPI for data
+* Will switch to openbb terminal and/or other more robust & real-time data sources
+
+### decision analysis plan
+* Begin with EMA
+* Explore ML and DL approaches
+* Incorporate NLP data from twitter and crypto news sources
+** incorporate general macro sentiment
+
--- src/main.py
+++ src/main.py
@@ -5,18 +5,19 @@ import requests
 import pycoingecko as pygc
 
 from ema_calculator import ema_calculatoor
+from coingecko import get_CGcoin_data
 
 ## TODO: Get data
 ## cg good for historical daily data
 cg = pygc.CoingGeckoAPI()
 
-coinlist = ['eth']
+coinlist = ['ethereum']
 
-timeframe = pd.date_range(start='1-1-2016',
-                       end='18-7-2022',
-                       freq='4H')
 
-eth = [cg.get_coin_history_by_id('ethereum', '-'.join(str(timeframe[i]).split(' ')[0].split('-')[::-1]))['market_data']['current_price']['usd'] for i in range(len(timeframe))]
+begin_date = '1-1-2016'
+end_date = '20-7-2022'
+freq = '4H'
+eth = get_CGcoin_data(begin_date,end_date,freq,'ethereum')
 
 df = pd.DataFrame({'date': timeframe,
                   'price': eth})
@@ -25,17 +26,34 @@ df.set_index('date', inplace=True)
 
 ## TODO: FUTURE:: Multivariate & NLP -> more data sources
 
-## TODO: perform analysis of data (LSTM, ema cross, etc.)
-alpha6 = np.divide(1.6)
+## TODO: perform analysis of data (ema cross -> LSTM -> beyond, etc.)
+alpha6 = np.divide(1,6)
 ema6 = ema_calculatoor(eth,alpha=alpha6)
 
 alpha9 = np.divide(1,9)
 ema9 = ema_calculatoor(eth,alpha=alpha9)
-        
 
+
+
+## TODO: return decision    
+flag = 0
+
+init_avg = np.mean(ema6[0],ema9[0])
+    
+for i,j in zip(ema6,ema9):
+    if i > j and flag == 0:
+        print('ema6 crosses ema9 - buy')
+        flag = 1
+    if i > j and flag == 1:
+        print('hold')
+    if i < j and flag == 0:
+        print('ema9 crosses ema6 - sell')
+        flag == 1
+    if i < j and flag == 0:
+        print('patience is a virtue or you can yolo')
     
 
-## TODO: return decision
+
 
 ## TODO: test decision // Sum of decisions
 
